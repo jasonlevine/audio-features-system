3f4ebc36bd168f98eb4b9a26a2fe319575716097
Jason Levine <jasonbeatboxer@gmail.com>
2013-11-06 17:51:27 -0500

    added track and mode selection

---
+++ src/audioAnalytics.cpp
@@ -0,0 +1,9 @@
+//
+//  audioAnalytics.cpp
+//  just_you
+//
+//  Created by Jason Levine on 2013-11-06.
+//
+//
+
+#include "audioAnalytics.h"
---
+++ src/audioAnalytics.h
@@ -0,0 +1,14 @@
+//
+//  audioAnalytics.h
+//  just_you
+//
+//  Created by Jason Levine on 2013-11-06.
+//
+//
+
+#ifndef __just_you__audioAnalytics__
+#define __just_you__audioAnalytics__
+
+#include <iostream>
+
+#endif /* defined(__just_you__audioAnalytics__) */
